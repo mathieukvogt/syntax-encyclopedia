61db42d9a2d457d05b838c2cbf9d51b3620a1055
Mathieu <149266208+mathieukvogt@users.noreply.github.com>
2024-01-15 01:34:57 +0100

    update script.js

--- script.js
+++ script.js
@@ -2,39 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
   let tl = gsap.timeline({ paused: true });
 
   tl.to(".menu-overlay", {
-    duration: 0.5,
+    duration: 0.75,
     clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
     ease: "power2.out",
   });
 
-  tl.from(
-    ".menu-link, .btn",
-    {
-      opacity: 0,
-      y: 60,
-      stagger: 0,
-      duration: 0.5,
-      ease: "power1.inOut",
-    },
-    "<"
-  );
-
   tl.to(
-    ".video-preview",
+    ".grid-item",
     {
-      duration: 0,
+      duration: 0.75,
       height: "200px",
       ease: "power2.out",
-    },
-    "<"
-  );
-
-  tl.to(
-    ".menu-divider",
-    {
-      duration: 0.5,
-      width: "100%",
-      ease: "power4.out",
     },
     "<"
   );
@@ -49,9 +27,10 @@ document.addEventListener("DOMContentLoaded", function () {
     tl.reverse();
   }
 
-  document.querySelector(".menu-open-btn").addEventListener("click", openMenu);
+  document.querySelector(".menu-btn-open").addEventListener("click", openMenu);
   document
-    .querySelector(".menu-close-btn")
+    .querySelector(".menu-btn-close")
     .addEventListener("click", closeMenu);
   tl.reverse();
 });
+
