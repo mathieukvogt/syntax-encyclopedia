7f9aeffa25612ccf233cccd21b883714c871051c
Mathieu <mathieukvogt@yahoo.com>
2024-01-21 02:31:02 +0100

    barba.js and gsap page transition

--- script.js
+++ script.js
@@ -56,3 +56,48 @@ function toggleDetail(detailId, event) {
     event.target.classList.add("expanded");
   }
 }
+
+/* BARBA PAGE TRANSITION */
+
+function pageTransition() {
+  var tl = gsap.timeline();
+
+  tl.to(".transition", {
+    duration: 1,
+    scaleY: 1,
+    transformOrigin: "bottom",
+    ease: "power4.inOut",
+  });
+
+  tl.to(".transition", {
+    duration: 1,
+    scaleY: 0,
+    transformOrigin: "top",
+    ease: "power4.inOut",
+    delay: 0.2,
+  });
+}
+
+function delay(n) {
+  n = n || 0;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+}
+
+barba.init({
+  sync: true,
+  transitions: [
+    {
+      async leave(data) {
+        const done = this.async();
+
+        pageTransition();
+        await delay(1000);
+        done();
+      },
+    },
+  ],
+});
